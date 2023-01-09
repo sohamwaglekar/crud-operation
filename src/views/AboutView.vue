07045d73316a4f80b9e168e8820e5189800b5baa
sohamwaglekar <som19waglekar@com>
2023-01-10 00:11:13 +0530

    first commit

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,67 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+   <input type="text" v-model="singlevalue.name" />
+   <input type="text" v-model="singlevalue.price" />
+   <input type="text" v-model="singlevalue.details" />
+  <button type="submit" @click="phoneadd">Submit</button>
   </div>
 </template>
+
+<script>
+import axios from 'axios'
+
+export default {
+ data() {
+  return {
+   
+    singlevalue:{
+       name: "",
+    price: "",
+    details: "",
+    }
+  }
+ },
+ methods:{
+
+  async phoneadd(){
+    let payload = {
+      name:this.singlevalue.name,
+      price:this.singlevalue.price,
+      details:this.singlevalue.details
+    }
+    if(this.$route.params.id) {
+      await axios.put("http://localhost:4000/api/products/"+this.$route.params.id, payload)
+    }
+    else{
+    try {
+     await axios.post("http://localhost:4000/api/products/createproducts", payload)
+     alert('success')
+    } catch (error) {
+      console.log(error)
+    }
+    }
+  },
+  async getsingle(){
+    try {
+     let singledata = await axios.get("http://localhost:4000/api/products/prodkey?productid="+this.$route.params.id )
+     this.singlevalue = singledata.data.data;
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+ },
+
+ computed:{
+  currentRoute(){
+    return this.$route;
+  }
+ },
+ async mounted(){
+  if(this.$route.params.id){
+    await this.getsingle()
+  }
+ }
+ 
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,47 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div>
+  <div v-for="(item,index) in viewdata" :key="'item-' + index" class="home">
+    <p>name: {{item.name}}</p>
+    <br />
+    <p>phone: {{item.price}}</p>
+     <br />
+    <p>details: {{item.details}}</p>
+    <button @click="updatebutton(item)">Update</button>
+    <button @click="deletebutton(item)">Delete</button>
   </div>
+
+ 
+</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
+
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+ data() {
+  return {
+    viewdata: ""
   }
+ },
+ methods:{
+  async getdata(){
+   let payload = await axios.get("http://localhost:4000/api/products")
+   this.viewdata = payload.data;
+   console.log(this.viewdata)
+  },
+  async updatebutton(item){
+    this.$router.push({ name: "about",params:{id:item._id}});
+
+  },
+  async deletebutton(item){
+    await axios.delete("http://localhost:4000/api/products/"+item._id)
+
+    await this.getdata()
+  }
+ },
+ mounted(){
+  this.getdata()
+ }
 }
 </script>
